c1dc909decb50a40166139fbf0a1fa16f5d542b7
AnxChow <ankitachowdhry97@gmail.com>
2017-04-17 17:57:04 -0500

    added my template schedule maker, app runs on local host and shows my template + react logo

--- sched/src/App.js
+++ sched/src/App.js
@@ -5,14 +5,42 @@ import './App.css';
 class App extends Component {
   render() {
     return (
+      // <div className="App">
+      // 	<div className="App-header">
+      //     Schedule Maker!
+      //   </div>
+      // 	<form id="survey" method="get">
+      // 		<div>
+      // 			<p> How much time do you have to work today?</p> <input type="text" name="total"> hours</input><br></br>
+      // 			<p> Do you want to take breaks?</p><br></br>
+      // 				<input type="radio" name="bre" id="yes">Yes</input>
+      // 				<input type="radio" name="bre" id="no">No</input>
+      // 			<p>If so, how long?</p>
+      // 				<input type="text" name="breaks"> minutes</input><br></br>
+      // 		</div> <br></br>
+      // 			<table id="activities">
+      // 				<tr>
+      // 					<th>Things to do: </th>
+      // 				</tr>
+      // 				<tr>
+      // 				<td>
+      // 					<label>Thing to do: </label><br/><input type="text" name="act1" class='activity'></input>
+      // 				</td>
+      //                 <td>
+      //                 	<label>Time (minutes): </label><br/><input type="text" name="time1" class='time'></input>
+      //                 </td>
+      //             	</tr>
+      //             </table>
+      //             <input type="submit" onClick="makeSched(this.form)"/>
+      //             <button type="button"id="add_activity">Add Activity</button>
+      // 	</form>
+      // </div>
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+          <h2></h2>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      
       </div>
     );
   }
--- sched/src/index.js
+++ sched/src/index.js
@@ -3,7 +3,19 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
 
+const element = React.createElement(
+  'h1',
+  {className: 'greeting'},
+  'Hello, world!'
+);
+
 ReactDOM.render(
+  //element,
   <App />,
   document.getElementById('root')
 );
+
+// ReactDOM.render(
+//   <h1>Hello, world!</h1>,
+//   document.getElementById('root')
+// );
